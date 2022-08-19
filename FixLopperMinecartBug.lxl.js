a0ed896f626c375f33b9af9d668433ae2367906a
xnhjs233 <111108745+xnhjs233@users.noreply.github.com>
2022-08-19 16:21:49 +0800

    Add files via upload

--- FixLopperMinecartBug.lxl.js
+++ FixLopperMinecartBug.lxl.js
@@ -1,9 +1,68 @@
-colorLog("red","刷物修复插件已启动")
+colorLog("red","刷物修复插件已启动");
 mc.listen("onJoin",function(pl){
-    pl.tell('§4刷物品方法已被修复')
-    mc.listen("onHopperSearchItem",function(pos,isMinecart){
-        if (isMinecart){
-            colorLog("red",'在'+ pos.dim + '坐标' + pos.x + ',' + pos.y + ',' + pos.z + '发现疑似刷物品行为');
-            return false
+    if (jf){
+        pl.tell("§4刷物品方法已被修复");
+    }
+})
+mc.listen("onHopperSearchItem",function (pos, isMinecart) {
+        if (isMinecart) {
+            if (kzt){
+                var pos1 = pos;
+                colorLog("red","在" + pos1 + "发现疑似刷物品行为");
+            }
+            return false;
         }
-})})
+})
+function isMenu(player) {
+    let cd = mc.newSimpleForm();
+    cd.setTitle("防刷物插件菜单");
+    cd.addButton("玩家进服提示选项");
+    cd.addButton("控制台输出选项");
+    player.sendForm(cd, (player, id) => {
+        if (id != null) {
+            switch (id) {
+                case 0:
+                    let cd = mc.newSimpleForm();
+                    cd.setTitle("防刷物插件菜单");
+                    cd.addButton("开启进服提示");
+                    cd.addButton("关闭进服提示");
+                    player.sendForm(cd, (player, id) => {
+                        if (id != null) {
+                            switch (id) {
+                                case 0:
+                                    jf = true;
+                                    break;
+                                case 1:
+                                    jf = false;
+                                    break;
+                            }
+                        }
+                    }
+                )
+                case 1:
+                    let cd1 = mc.newSimpleForm();
+                    cd1.setTitle("防刷物插件菜单");
+                    cd1.addButton("开启控制台提示");
+                    cd1.addButton("关闭控制台提示");
+                    player.sendForm(cd1, (player,id) => {
+                        if (id != null) {
+                            switch (id){
+                                case 0:
+                                    kzt = true;
+                                    break;
+                                case 1:
+                                    kzt = false;
+                                    break;
+                                    }
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    )
+}
+mc.regPlayerCmd("fsw", "防刷物插件菜单", isMenu,1);
+function jf(){};
+function pos1(){};
+function kzt(){};
